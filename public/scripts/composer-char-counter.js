940c8bef93b4b808ffdada387542dd478ad36a5f
Josh Whelan <josh.d.whelan@gmail.com>
2019-03-19 23:52:48 +0000

    added jQuery to handle counter; linked to html

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  
+  $('.text-place').on('keyup', function () {
+    maxChar = 140;
+    counter = this.value.length;
+    count =  maxChar - counter;
+
+    $('.counter').text(count);
+    
+    if (count < 0) {
+      $('.counter').css('color', 'red');
+    } else {
+      $('.counter').css('color', 'black');
+    }
+  });
+});
+
